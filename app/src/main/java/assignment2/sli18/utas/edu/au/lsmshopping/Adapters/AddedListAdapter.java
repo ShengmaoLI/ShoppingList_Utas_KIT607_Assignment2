0a21267fc10e725b0fd4ffa2e3ecc0ee6b028fba
Shengmao Li <shengmaoli@10-200-209-222-eduroam.staff.utas.edu.au>
2018-05-15 20:50:18 +1000

    我的笔记本电脑崩溃前

---
+++ app/src/main/java/assignment2/sli18/utas/edu/au/lsmshopping/Adapters/AddedListAdapter.java
@@ -0,0 +1,6 @@
+//package assignment2.sli18.utas.edu.au.lsmshopping.Adapters;
+//
+//import android.support.v7.widget.RecyclerView;
+//
+//public class AddedListAdapter extends RecyclerView.Adapter {
+//}
---
+++ app/src/main/java/assignment2/sli18/utas/edu/au/lsmshopping/Adapters/PurchasedListAdapter.java
@@ -0,0 +1,16 @@
+//package assignment2.sli18.utas.edu.au.lsmshopping.Adapters;
+//
+//import android.support.v7.widget.RecyclerView;
+//
+//import java.util.List;
+//
+//import Entity.ShoppingItem;
+//
+//public class PurchasedListAdapter extends RecyclerView.Adapter<> {
+//    private List<ShoppingItem> mShoppingItems;
+//    static class ViewHolder extends RecyclerView.ViewHolder{
+//        public ViewHolder(View view){
+//
+//        }
+//    }
+//}
